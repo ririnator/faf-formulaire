dd713d6b37a4698b8cc10def277a98ee3ba71dd9
Irina <irinasendrison@gmail.com>
2025-07-16 16:55:56 +0200

    changement du nom des variables d'environnement

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -2,9 +2,9 @@
 const { v2: cloudinary } = require('cloudinary');
 
 cloudinary.config({
-  cloud_name:    process.env.CLOUDINARY_CLOUD,   // ex: "mon-cloud"
-  api_key:       process.env.CLOUDINARY_KEY,     // ta clé API
-  api_secret:    process.env.CLOUDINARY_SECRET,  // ton secret API
+  cloud_name:    process.env.CLOUDINARY_CLOUD_NAME,   // ex: "mon-cloud"
+  api_key:       process.env.CLOUDINARY_API_KEY,     // ta clé API
+  api_secret:    process.env.CLOUDINARY_API_SECRET,  // ton secret API
   secure:        true
 });
 
